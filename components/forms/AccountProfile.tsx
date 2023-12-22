0308c30e93148478691185907050093d6b071c89
albert19_ <albertnanda1902@gmail.com>
2023-12-22 20:05:39 +0800

    create uploadthing api and utils

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,7 @@ import * as z from "zod"
 import Image from 'next/image';
 import { ChangeEvent, useState } from 'react';
 import { isBase64Image } from '@/lib/utils';
+import { useUploadThing } from '@/lib/uploadthing';
 
 interface Props {
     user: {
@@ -73,6 +74,10 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         const blob = values.profile_photo;
 
         const hasImageChanged = isBase64Image(blob);
+
+        if (hasImageChanged) {
+            // const imgRed = 
+        }
     }
 
     return (
