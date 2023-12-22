ad8271815d1e6a7690536eefc0954a21d03f71b3
albert19_ <albertnanda1902@gmail.com>
2023-12-22 19:55:52 +0800

    updat utils functions

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,7 +18,8 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { UserValidation } from '@/lib/validations/user';
 import * as z from "zod"
 import Image from 'next/image';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
+import { isBase64Image } from '@/lib/utils';
 
 interface Props {
     user: {
@@ -34,6 +35,8 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
 
+    const [files, setFiles] = useState<File[]>([]);
+
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
@@ -44,12 +47,32 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         }
     });
 
-    const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
         e.preventDefault();
+
+        const fileReader = new FileReader();
+
+        if (e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0];
+
+            setFiles(Array.from(e.target.files));
+
+            if (!file.type.includes('image')) return;
+
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || "";
+
+                fieldChange(imageDataUrl);
+            }
+
+            fileReader.readAsDataURL(file);
+        }
     }
 
     function onSubmit(values: z.infer<typeof UserValidation>) {
-        console.log(values)
+        const blob = values.profile_photo;
+
+        const hasImageChanged = isBase64Image(blob);
     }
 
     return (
@@ -71,7 +94,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                                         width={96}
                                         height={96}
                                         priority
-                                        className='rounded-full object-contain'
+                                        className='w-full h-full overflow-hidden object-cover rounded-full'
                                     />
                                 ) : (
                                     <Image
