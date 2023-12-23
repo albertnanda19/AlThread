4618b1faa02ba1a2f230856cb06dd41fa1774c6d
albert19_ <albertnanda1902@gmail.com>
2023-12-24 03:08:38 +0800

    create fetch activity function on user actions

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,4 +1,16 @@
-const Page = () => {
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs"
+import { redirect } from 'next/navigation'
+
+const Page = async () => {
+
+    const user = await currentUser();
+
+    if (!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+
+    if (!userInfo.onboarded) redirect('/onboarding');
     return (
         <section>
             <h1 className="head-text mb-10">Activity</h1>
