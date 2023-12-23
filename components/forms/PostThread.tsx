49bd03e36ca5c3a6b4496e67e8d266db26fa33cb
albert19_ <albertnanda1902@gmail.com>
2023-12-23 14:15:31 +0800

    create thread form input

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -43,8 +43,41 @@ const PostThread = ({ userId }: { userId: string }) => {
             accountId: userId,
         }
     });
+
+    const onSubmit = () => {
+
+    }
+
     return (
-        <h1>Post Thread Form</h1>
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="mt-10 flex flex-col justify-start gap-10"
+            >
+                <FormField
+                    control={form.control}
+                    name="thread"
+                    render={({ field }) => (
+                        <FormItem className='flex flex-col gap-3 w-full'>
+                            <FormLabel className='text-base-semibold text-light-2'>
+                                Content
+                            </FormLabel>
+                            <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
+                                <Textarea
+                                    rows={15}
+                                    className='account-form_input no-focus'
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type='submit' className='bg-primary-500'>
+                    Post Thread
+                </Button>
+            </form>
+        </Form>
     )
 }
 
