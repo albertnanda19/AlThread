bde7ca35a0fbf4ebca5f510eebe4be75672adf47
albert19_ <albertnanda1902@gmail.com>
2023-12-24 01:28:07 +0800

    create fetch users on user actions

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,4 +1,20 @@
-const Page = () => {
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs"
+import { redirect } from 'next/navigation'
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+
+const Page = async () => {
+
+    const user = await currentUser();
+
+    if (!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+
+    if (!userInfo.onboarded) redirect('/onboarding');
     return (
         <section>
             <h1 className="head-text mb-10">Search</h1>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface Params {
     userId: string;
@@ -26,7 +27,7 @@ export async function updateUser({
 
     try {
         await User.findOneAndUpdate(
-            {id: userId},
+            { id: userId },
             {
                 username: username.toLowerCase(),
                 name,
@@ -34,10 +35,10 @@ export async function updateUser({
                 image,
                 onboarded: true,
             },
-            {upsert: true}
+            { upsert: true }
         );
-    
-        if(path === '/profile/edit') {
+
+        if (path === '/profile/edit') {
             revalidatePath(path);
         }
     } catch (error: any) {
@@ -50,11 +51,11 @@ export async function fetchUser(userId: string) {
         connectToDB();
 
         return await User
-            .findOne({id: userId})
-            // .populate({
-            //     path: 'communities',
-            //     model: Community
-            // })
+            .findOne({ id: userId })
+        // .populate({
+        //     path: 'communities',
+        //     model: Community
+        // })
 
     } catch (error: any) {
         throw new Error(`Failed to fetch user: ${error.message}`);
@@ -65,7 +66,7 @@ export async function fetchUserPosts(userId: string) {
     try {
         connectToDB();
 
-        const threads = await User.findOne({id: userId})
+        const threads = await User.findOne({ id: userId })
             .populate({
                 path: 'threads',
                 model: Thread,
@@ -80,9 +81,60 @@ export async function fetchUserPosts(userId: string) {
                 }
             })
 
-            return threads;
+        return threads;
 
     } catch (error: any) {
         throw new Error(`Failed to fetch user posts: ${error.message}`);
+    }
+}
+
+export async function fetchUsers({
+    userId,
+    searchString = "",
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = "desc"
+}: {
+    userId: string;
+    searchString?: string;
+    pageNumber?: number;
+    pageSize?: number;
+    sortBy?: SortOrder;
+}) {
+    try {
+        connectToDB();
+
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        const regex = new RegExp(searchString, "i");
+
+        const query: FilterQuery<typeof User> = {
+            id: { $ne: userId }
+        }
+
+        if (searchString.trim() !== '') {
+            query.$or = [
+                { username: { $regex: regex } },
+                { name: { $regex: regex } }
+            ]
+        }
+
+        const sortOptions = { createdAt: sortBy };
+
+        const usersQuery = User.find(query)
+            .sort(sortOptions)
+            .skip(skipAmount)
+            .limit(pageSize);
+
+        const totalUSersCount = await User.countDocuments(query);
+
+        const users = await usersQuery.exec();
+
+        const isNext = totalUSersCount > skipAmount + users.length;
+
+        return { users, isNext }
+
+    } catch (error: any) {
+        throw new Error(`Failed to fetch users: ${error.message}`)
     }
 }
