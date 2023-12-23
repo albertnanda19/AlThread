ff698cabcd283c858769087d5ac55ae38c0c7736
albert19_ <albertnanda1902@gmail.com>
2023-12-23 16:13:23 +0800

    create thread action to store thread into database

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
@@ -11,17 +13,23 @@ interface Params {
 }
 
 export async function createThread({text, author, communityId, path}: Params) {
-    connectToDB();
 
-    const createdThread = await Thread.create({
-        text,
-        author,
-        community: null,
-    });
+    try {
+        connectToDB();
+    
+        const createdThread = await Thread.create({
+            text,
+            author,
+            community: null,
+        });
+    
+        await User.findByIdAndUpdate(author, {
+            $push: {threads: createdThread._id},
+        })
+    
+        revalidatePath(path);
+    } catch (error: any) {
+        throw new Error(`Error creating thread: ${error.message}`);
+    }
 
-    await User.findByIdAndUpdate(author, {
-        $push: {threads: createdThread._id},
-    })
-
-    revalidatePath(path);
 }
